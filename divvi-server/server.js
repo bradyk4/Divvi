6c8b8b51c5af8b3b61e6163c90d6f3a0a70116f0
bradyk4 <bradyk4@mail.uc.edu>
2021-11-02 11:21:44 -0400

    modify server for sequelize

--- divvi-server/server.js
+++ divvi-server/server.js
@@ -1,41 +1,12 @@
-const dbOperations = require('./config/dbOperations');
-var DB = require('./config/dbOperations');
-var User = require('./models/user')
+const { ConnectionPool } = require('mssql');
+const { Sequelize } = require('sequelize');
+const config = require('./config/dbConfig');
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var app = express();
-var router = express.Router();
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
-app.use('/api', router);
-
-router.use((request,response,next)=>{
-  console.log('middleware');
-  next();
-});
-
-router.route('/users').get((request, response)=>{
-
-  DB.getUsers().then(result => {
-    response.json(result);
-  });
-
-});
-
-router.route('/users').post((request, response)=>{
-
-  let user = {...request.body}
-
-  DB.addUser(user).then(result => {
-    response.status(201).json(result);
-  });
-
-});
-
-var port = process.env.PORT || 8090;
-app.listen(port);
-console.log('API is runnning at ' + port);
+var connection = new ConnectionPool(config)
+  .connect()
+  .then(pool => {
+    console.log(pool);
+  })
+  .catch(err => {
+    console.log(err);
+  })
