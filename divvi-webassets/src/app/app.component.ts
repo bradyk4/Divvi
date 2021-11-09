9e0652fd7bb05b5c60ffa04ad386b569e8722174
bradyk4 <bradyk4@mail.uc.edu>
2021-11-09 09:58:39 -0500

    add user & group services with functions for all actions needed

--- divvi-webassets/src/app/app.component.ts
+++ divvi-webassets/src/app/app.component.ts
@@ -5,6 +5,8 @@ import { UserCreationComponent } from './user-creation/user-creation.component';
 import {MatDialogModule} from "@angular/material/dialog";
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import {HttpClient} from '@angular/common/http';
+import { UserService } from './services/user.service';
+import { GroupService } from './services/group.service';
 
 
 @Component({
@@ -14,17 +16,19 @@ import {HttpClient} from '@angular/common/http';
 })
 
 export class AppComponent implements OnInit{
-  constructor(private http : HttpClient) {}
+  constructor(private http : HttpClient, private userService: UserService, private groupService: GroupService) {}
 
   title = 'Divvi';
-  users: any = [];
-  groups: any = [];
-  groupUsers: any = [];
+  user: any;
+  users: any;
+  selectedGroup: any;
+  groups: any;
+  groupUsers: any;
 
   ngOnInit(): void {
-    this.getUsers();
-    this.getGroups();
-    this.getGroupUsers(1);
+    this.users = this.getUsers();
+    this.groups = this.getGroups();
+    this.groupUsers = this.getGroupUsers(1);
   }
 
 public group: Array<{username: string, amountOwed: number}> = [];
@@ -100,28 +104,51 @@ createTable(){
     this.group.length = 0;
   }
 
-  // API response handling
+  // setup user API functions
   getUsers(){
-    this.http.get('http://localhost:8090/api/users')
-      .subscribe(data => {
-        console.log(data);
-        this.users = data;
-      });
+    this.userService.getUsers().subscribe(data => {
+      this.users = data;
+      return this.users;
+    });
   }
 
+  getUserById(id:number){
+    this.userService.getUserById(id).subscribe(data => {
+      this.user = data;
+      return this.user;
+    })
+  }
+
+  postUser(name: string, balance: number, groupId: number, id: number){
+    this.userService.postUser(name, balance, groupId, id);
+  }
+
+  updateUserBalance(id: number, balance: number){
+    this.userService.updateUserBalance(id, balance);
+  }
+
+  deleteUser(id: number){
+    this.userService.deleteUser(id);
+  }
+
+  // setup group API functions
   getGroups(){
-    this.http.get('http://localhost:8090/api/groups')
-      .subscribe(data => {
-        console.log(data);
-        this.groups = data;
-      })
+    this.groupService.getGroups().subscribe(data => {
+      this.groups = data;
+      return this.groups;
+    });
   }
 
   getGroupUsers(id: number){
-    this.http.get('http://localhost:8090/api/groups/users/' + id)
-      .subscribe(data => {
-        console.log(data);
-        this.groupUsers = data;
-      });
+    this.groupService.getGroupUsers(id).subscribe(data => {
+      this.groupUsers = data;
+    });
   }
+
+  getGroup(id: number){
+    this.groupService.getGroup(id).subscribe(data => {
+      this.selectedGroup = data;
+    })
+  }
+
 }
---
+++ divvi-webassets/src/app/services/user.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+const baseURL = "http://localhost:8090/";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http : HttpClient) { }
+
+  // API response handling
+  getUsers(){
+    return this.http.get('http://localhost:8090/api/users');
+  }
+
+  getUserById(id: number){
+    return this.http.get('http://localhost:8090/api/users/'+id);
+  }
+
+  postUser(name: string, balance: number, groupId: number, id: number){
+    const body = {
+      "name": name,
+      "balance": balance,
+      "groupId": groupId,
+      "id": id
+    }
+    return this.http.post('http://localhost:8090/api/users/postUser', body)
+  }
+
+  updateUserBalance(id: number, balance: number){
+    const body = { "balance": balance };
+    return this.http.put('http://localhost:8090/api/users/'+id, body)
+  }
+
+  deleteUser(id: number){
+    return this.http.delete('http://localhost:8090'+id);
+  }
+}
