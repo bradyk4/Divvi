505f0dfa4a02e0e6f07a7dab84f6062f2b66073c
bradyk4 <bradyk4@mail.uc.edu>
2022-03-23 04:27:16 -0400

    add functionality to signup to assign group name to group id

--- divvi-webassets/src/app/sign-up-page/sign-up/sign-up.component.ts
+++ divvi-webassets/src/app/sign-up-page/sign-up/sign-up.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { UserService } from 'src/app/services/user.service';
 import {Router} from '@angular/router';
+import { GroupService } from 'src/app/services/group.service';
 
 
 
@@ -15,16 +16,27 @@ export class SignUpComponent implements OnInit {
   username: any;
   password: any;
   groupName: any;
+  groups: any;
+  groupId: any;
 
   constructor(
     private http:HttpClient,
     private userService: UserService,
+    private groupService: GroupService,
     private router:Router
   ) { }
 
   ngOnInit(): void {
+    this.groups = this.getGroups();
   }
 
+  // setup group API functions
+  getGroups(){
+    this.groupService.getGroups().subscribe(data => {
+      this.groups = data;
+      return this.groups;
+    });
+  }
 
   onBack() {
     this.router.navigate(['/login']);
@@ -32,8 +44,21 @@ export class SignUpComponent implements OnInit {
   onSubmit() {
     this.username = this.username.toString();
     this.password = this.password.toString();
-    let groupId: number = 1;
-    this.userService.postUser(this.username, this.password, 0, groupId).subscribe();
+    this.groupName = this.groupName.toString();
+
+    //assign group name to group id for user creation
+    this.groups.forEach((group: any) => {
+      // checks that group exists
+      if (this.groupName == group.name)
+      {
+        this.groupId = group.id;
+        console.log(this.username, this.password, 0, this.groupId);
+        this.userService.postUser(this.username, this.password, 0, this.groupId).subscribe();
+      }
+      else{
+        console.error("No group exists with that name.");
+      }
+    });
 
     this.router.navigate(['/login']);
   }
